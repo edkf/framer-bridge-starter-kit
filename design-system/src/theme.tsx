cb7734edd503e4a220fab58ff42425d17a4b9408
Floris Verloop <floris@framer.com>
2019-04-15 16:26:27 +0200

    Clean up theme

--- design-system/src/theme.tsx
+++ design-system/src/theme.tsx
@@ -1,50 +1,23 @@
-export enum Color {
-  /** Used for general pane elements like inputs & buttons */
-  paneBg = "paneBg",
-
-  /** Used for general pane elements like inputs & buttons */
-  paneBgDark = "paneBgDark",
-
-  /** Used for general pane elements like inputs & buttons */
-  paneBorder = "paneBorder",
-
-  /** Primary brand color */
-  Primary = "primary",
-
-  /** Primary brand color for dark accents */
-  PrimaryDark = "primaryDark",
-
-  /** Primary brand color for light accents */
-  PrimaryLight = "primaryLight",
-
-  /** Used for errors and breaking changes */
-  Danger = "danger",
-
-  /** Used for errors and breaking changes */
-  DangerDark = "dangerDark",
-
-  /** Used for errors and breaking changes */
-  DangerLight = "dangerLight",
-
-  /** Used for default body text */
-  Body = "body",
-
-  /** Used for light body text */
-  BodyLight = "bodyLight",
-
-  /** Used for lighter body text */
-  BodyLighter = "bodyLighter",
-
-  /** Used for lightest body text */
-  BodyLightest = "bodyLightest"
+type Colors = {
+  paneBg: string
+  paneBgDark: string
+  paneBorder: string
+  primary: string
+  primaryDark: string
+  primaryLight: string
+  danger: string
+  dangerDark: string
+  dangerLight: string
+  body: string
+  bodyLight: string
+  bodyLighter: string
+  bodyLightest: string
 }
-
-type Colors = { [key in Color]: string }
 
 const color: Colors = {
   paneBg: "#FFF",
   paneBgDark: "#FDFDFD",
-  paneBorder: "rgba(0,0,0,.1)",
+  paneBorder: "rgba(0, 0, 0, 0.1)",
   primary: "#75C",
   primaryDark: "#74C",
   primaryLight: "#EEF",
@@ -63,21 +36,7 @@ const space: string[] = ["0", "4px", "8px", "16px", "32px"]
 
 const font = "-apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif"
 
-type Theme = {
-  /** Brand colors */
-  color: Colors
-
-  /** Brand font family */
-  font: string
-
-  /** Font sizes */
-  size: string[]
-
-  /** Spacing sizes used for margins and paddings */
-  space: string[]
-}
-
-export const theme: Theme = {
+export const theme = {
   color,
   font,
   size,
